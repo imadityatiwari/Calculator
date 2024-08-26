64ab8b04947ae8c936cb1419f4e3703b31aed18f
Aditya Kumar Tiwari <aditya.tiwari@girikon.com>
2024-08-26 13:12:40 +0530

    removed unwanted text

--- src/App.jsx
+++ src/App.jsx
@@ -32,13 +32,3 @@ function App() {
 }
 
 export default App;
-
-
-https://teams.microsoft.com/l/channel/19%3Abb173e002e724402b93fbcb850f653ad%40thread.tacv2/Learning?groupId=c97032cc-6e97-4c2d-b771-be84944f20e7&tenantId=e6191525-9bc8-4230-bd08-2a1826f850fe
-
-
-https://teams.microsoft.com/l/team/19%3AJ5SxwixYFdkZLFtE4gRaj5YPrMK23WaZCLM3vQeWRBM1%40thread.tacv2/conversations?groupId=c97032cc-6e97-4c2d-b771-be84944f20e7&tenantId=e6191525-9bc8-4230-bd08-2a1826f850fe
-
-
-19%3Abb173e002e724402b93fbcb850f653ad%40thread.tacv2
-19%3AJ5SxwixYFdkZLFtE4gRaj5YPrMK23WaZCLM3vQeWRBM1%40thread.tacv2
